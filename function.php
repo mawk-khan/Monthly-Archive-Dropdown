c6ad746439b9be2eed2639af7f36f9fecad3068b
Wajid Khan <mawk-khan@users.noreply.github.com>
2022-12-08 15:45:22 +0530

    Update function.php

--- function.php
+++ function.php
@@ -10,8 +10,8 @@
  * By: Wajid Khan
  */
 function exponent_archives_shortcode( $atts ) { ?>
-    <div class="exp-archive taxonomy-list widget taxonomy-category">
-        <select class="exp-archive-dropdown" name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+    <div class="monthly-archive">
+        <select class="monthly-archive-dropdown" name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
             <option value=""><?php echo esc_attr( __( 'Monthly Archives' ) ); ?></option> 
             <?php wp_get_archives( array( 'type' => 'monthly', 'format' => 'option' ) ); ?>
         </select>
